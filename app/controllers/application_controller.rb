1e37d572b53efa05050d0bd2309c83870f66b04b
guillaume imary <guillaume.imary@gmail.com>
2020-10-01 17:05:26 +0200

    Peut modifier un user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    attributes = [:nom, :prenom, :email, :password, :club, :pays]
+    attributes = [:nom, :prenom, :email, :password, :club, :pays, :current_password]
     devise_parameter_sanitizer.permit(:sign_in, keys: attributes)
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
     devise_parameter_sanitizer.permit(:account_update, keys: attributes)
--- spec/features/user_request_spec.rb
+++ spec/features/user_request_spec.rb
@@ -14,32 +14,31 @@ RSpec.describe "Users" do
     end
 
     it "crée son compte sans erreur" do
-        expect do
-            visit new_user_registration_path
+      expect do
+        visit new_user_registration_path
 
-            fill_in :user_email, with: 'jean@bon.fr'
-            fill_in :user_password, with: 'password'
-            fill_in :user_password_confirmation, with: 'password'
-            fill_in :user_nom, with: 'Bon'
-            fill_in :user_prenom, with: 'Jean'
-            fill_in :user_club, with: 'Beurre'
-            fill_in :user_pays, with: 'France'
-            click_on 'Sign up'
-        end.to change(User, :count)
-        user = User.last
-        expect(user.nom).to eq 'Bon'
-        expect(user.prenom).to eq 'Jean'
-        expect(user.club).to eq 'Beurre'
-        expect(user.pays).to eq 'France'
-        expect(user.email).to eq 'jean@bon.fr'
+        fill_in :user_email, with: 'jean@bon.fr'
+        fill_in :user_password, with: 'password'
+        fill_in :user_password_confirmation, with: 'password'
+        fill_in :user_nom, with: 'Bon'
+        fill_in :user_prenom, with: 'Jean'
+        fill_in :user_club, with: 'Beurre'
+        fill_in :user_pays, with: 'France'
+        click_on 'Sign up'
+      end.to change(User, :count)
+      user = User.last
+      expect(user.nom).to eq 'Bon'
+      expect(user.prenom).to eq 'Jean'
+      expect(user.club).to eq 'Beurre'
+      expect(user.pays).to eq 'France'
+      expect(user.email).to eq 'jean@bon.fr'
     end
   end
 
-  describe 'quand un utilisateur modifie son compte' do
-    let(:user2) { User.create(nom: 'Hanin', prenom: 'Roger', club: 'PJ', pays: 'France', email: 'rh@pj.fr', password: 'password', password_confirmation: 'password')}
-    let(:guitou) {create(:user)}
+  fdescribe 'quand un utilisateur modifie son compte' do
+    let!(:guitou) {create(:user)}
 
-    fit 'affiche les bons champs' do
+    it 'affiche les bons champs' do
       se_connecte_en_tant_qu_user(guitou)
       visit edit_user_registration_path(guitou)
 
@@ -49,27 +48,25 @@ RSpec.describe "Users" do
       expect(page).to have_content 'Pays'
       expect(page).to have_content 'E-mail'
       expect(page).to have_content 'Mot de passe'
+      expect(guitou.club).to eq 'SSA'
     end
 
     it "modifie les champs sans erreur" do
       expect do
-          visit new_user_registration_path
+        se_connecte_en_tant_qu_user(guitou)
+        visit edit_user_registration_path(guitou)
 
-          fill_in :user_email, with: 'jean@bon.fr'
-          fill_in :user_password, with: 'password'
-          fill_in :user_password_confirmation, with: 'password'
-          fill_in :user_nom, with: 'Bon'
-          fill_in :user_prenom, with: 'Jean'
-          fill_in :user_club, with: 'Beurre'
-          fill_in :user_pays, with: 'France'
-          click_on 'Sign up'
-      end.to change(User, :count)
+        fill_in :user_current_password, with: 'password'
+        fill_in :user_club, with: 'Beurre'
+        fill_in :user_pays, with: 'Monaco'
+        click_on 'Update'
+      end.to_not change(User, :count)
+
       user = User.last
-      expect(user.nom).to eq 'Bon'
-      expect(user.prenom).to eq 'Jean'
+      expect(user.nom).to eq 'Chou'
+      expect(user.prenom).to eq 'Guitou'
       expect(user.club).to eq 'Beurre'
-      expect(user.pays).to eq 'France'
-      expect(user.email).to eq 'jean@bon.fr'
+      expect(user.pays).to eq 'Monaco'
     end
   end
 end
